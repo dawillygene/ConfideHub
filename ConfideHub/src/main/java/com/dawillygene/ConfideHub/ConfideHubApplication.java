771d7a90caf678371f600411c8397216b0edb408
dawillygene <eliamariki2000@gmail.com>
2025-05-10 21:24:36 +0300

    initial backend

---
+++ ConfideHub/src/main/java/com/dawillygene/ConfideHub/ConfideHubApplication.java
@@ -0,0 +1,13 @@
+package com.dawillygene.ConfideHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfideHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfideHubApplication.class, args);
+	}
+
+}
---
+++ ConfideHub/src/test/java/com/dawillygene/ConfideHub/ConfideHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawillygene.ConfideHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfideHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
